faa8d7b1bdbe492a1da6b34be50e4e12da14fa7a
Tobias Groschup <tgroschup@gmail.com>
2017-12-02 11:22:39 +0100

    More elegant solution to day one.

--- src/main/scala/CaptchaSolver.scala
+++ src/main/scala/CaptchaSolver.scala
@@ -1,5 +1,10 @@
 class CaptchaSolver(input: String) {
-    val numbers: List[Int] = input.map(_.toInt - 48).toList :+ input(0).toInt - 48
+    val numbers: List[Int] = input.map(c => java.lang.Integer.parseInt(c.toString)).toList
 
-    def sum: Int = numbers.sliding(2).filter(pair => pair(0) == pair(1)).map(_.head).sum
+    def sum1: Int = (numbers :+ numbers.head).sliding(2).filter(pair => pair(0) == pair(1)).map(_.head).sum
+
+    def sum2: Int = {
+        val rotated = numbers.slice(numbers.length/2, numbers.length) ::: numbers.slice(0, numbers.length/2)
+        numbers.zip(rotated).filter(pair => pair._1 == pair._2).map(_._1).sum
+    }
 }
