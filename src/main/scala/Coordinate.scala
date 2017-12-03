26f0ae1bb6c32fb65b8575034081ba0fa75a13d8
Tobias Groschup <tgroschup@gmail.com>
2017-12-03 16:53:22 +0100

    Added solution for day three.

--- src/main/scala/Coordinate.scala
+++ src/main/scala/Coordinate.scala
@@ -1,11 +1,87 @@
+import scala.collection.mutable
+
 object Coordinate {
-    def fromSpiralCoordinate(position: Int): Coordinate = {
-        val x = 0
-        val y = 0
-        Coordinate(x, y)
+    type ValueMap = mutable.Map[Coordinate, Option[Int]]
+
+    val m: ValueMap = mutable.Map(Coordinate(0,0) -> Some(1))
+
+    private def updateValueMap(c: Coordinate): Unit = {
+
+        val surroundingValues: List[Option[Int]] = List(m.getOrElse(c.right, None), m.getOrElse(c.up, None)
+            , m.getOrElse(c.left, None), m.getOrElse(c.down, None), m.getOrElse(c.right.up, None)
+            , m.getOrElse(c.up.left, None), m.getOrElse(c.left.down, None), m.getOrElse(c.down.right, None))
+
+        val newVal: Option[Int] = Some(surroundingValues.map(_.getOrElse(0)).sum)
+
+        m += c -> newVal
     }
+
+    private def goUp(c: Coordinate, currentSize: Int, currentPosition: Int, toPosition: Int): Coordinate = {
+        updateValueMap(c)
+        if(currentPosition == toPosition) c
+        else if(c.isRightUpCorner(currentSize)) goLeft(c.left, currentSize, currentPosition+1, toPosition)
+        else goUp(c.up, currentSize, currentPosition+1, toPosition)
+    }
+    private def goDown(c: Coordinate, currentSize: Int, currentPosition: Int, toPosition: Int): Coordinate = {
+        updateValueMap(c)
+        if(currentPosition == toPosition) c
+        else if(c.isLeftDownCorner(currentSize)) goRight(c.right, currentSize, currentPosition+1, toPosition)
+        else goDown(c.down, currentSize, currentPosition+1, toPosition)
+    }
+    private def goRight(c: Coordinate, currentSize: Int, currentPosition: Int, toPosition: Int): Coordinate = {
+        updateValueMap(c)
+        if(currentPosition == toPosition) c
+        else if (c.isRightDownCorner(currentSize)) nextSpiral(c.right, currentSize, currentPosition+1, toPosition)
+        else goRight(c.right, currentSize, currentPosition+1, toPosition)
+    }
+    private def goLeft(c: Coordinate, currentSize: Int, currentPosition: Int, toPosition: Int): Coordinate = {
+        updateValueMap(c)
+        if(currentPosition == toPosition) c
+        else if(c.isLeftUpCorner(currentSize)) goDown(c.down, currentSize, currentPosition+1, toPosition)
+        else goLeft(c.left, currentSize, currentPosition+1, toPosition)
+    }
+
+    private def nextSpiral(c: Coordinate, currentSize: Int, currentPosition: Int, toPosition: Int): Coordinate = {
+        updateValueMap(c)
+        if(currentPosition == toPosition) c
+        else goUp(c.up, currentSize+1, currentPosition+1, toPosition)
+    }
+
+    private def enumerateInSpiral(toPosition: Int): Coordinate = {
+        m.clear
+        m += (Coordinate(0,0) -> Some(1))
+
+        if(toPosition == 1) Coordinate(0,0)
+        else goUp(Coordinate(0,0).right, 1, 2, toPosition)
+    }
+
+    def fromSpiralCoordinate(position: Int): Coordinate = enumerateInSpiral(position)
+
 }
 
 case class Coordinate(x: Int, y: Int) {
-    def mannheimDistanceFromOrigin: Int = x + y
+    def mannheimDistanceFromOrigin: Int = math.abs(x) + math.abs(y)
+
+    def isRightDownCorner(size: Int): Boolean = {
+        x == size && y == -1 * size
+    }
+
+    def isRightUpCorner(size: Int): Boolean = {
+        x == size && y == size
+    }
+
+    def isLeftUpCorner(size: Int): Boolean = {
+        -1 * x == size && y == size
+    }
+
+    def isLeftDownCorner(size: Int): Boolean = {
+        x == -1 * size && y == -1 * size
+    }
+
+    def up: Coordinate = Coordinate(x, y+1)
+    def down: Coordinate = Coordinate(x, y-1)
+    def left: Coordinate = Coordinate(x-1, y)
+    def right: Coordinate= Coordinate(x+1, y)
+
+    def getValue: Int = Coordinate.m(this).get
 }
--- src/test/scala/ExecuteDay3.scala
+++ src/test/scala/ExecuteDay3.scala
@@ -1,11 +1,36 @@
 import org.scalatest.FunSuite
 
 class ExecuteDay3 extends FunSuite{
+    val input = 347991
+
     test("origin") {
         assertResult(0)(Coordinate.fromSpiralCoordinate(1).mannheimDistanceFromOrigin)
     }
 
+    test("first circle") {
+        assertResult(Coordinate(1,0))(Coordinate.fromSpiralCoordinate(2)) //go up
+        assertResult(Coordinate(1,1))(Coordinate.fromSpiralCoordinate(3)) //go left
+        assertResult(Coordinate(0,1))(Coordinate.fromSpiralCoordinate(4)) //go left
+        assertResult(Coordinate(-1,1))(Coordinate.fromSpiralCoordinate(5)) //go down
+        assertResult(Coordinate(-1,0))(Coordinate.fromSpiralCoordinate(6)) //go down
+        assertResult(Coordinate(-1,-1))(Coordinate.fromSpiralCoordinate(7))
+        assertResult(Coordinate(0,-1))(Coordinate.fromSpiralCoordinate(8))
+        assertResult(Coordinate(1,-1))(Coordinate.fromSpiralCoordinate(9))
+    }
+
+    test("transition to and first step in second circle") {
+        assertResult(Coordinate(2,-1))(Coordinate.fromSpiralCoordinate(10))
+        assertResult(Coordinate(2,0))(Coordinate.fromSpiralCoordinate(11))
+    }
+
+    test("critical corners") {
+        assertResult(Coordinate(2,2))(Coordinate.fromSpiralCoordinate(13))
+        assertResult(Coordinate(1,2))(Coordinate.fromSpiralCoordinate(14))
+        assertResult(Coordinate(-2,2))(Coordinate.fromSpiralCoordinate(17))
+    }
+
     test("12") {
+        assertResult(Coordinate(2,1))(Coordinate.fromSpiralCoordinate(12))
         assertResult(3)(Coordinate.fromSpiralCoordinate(12).mannheimDistanceFromOrigin)
     }
 
@@ -17,7 +42,31 @@ class ExecuteDay3 extends FunSuite{
         assertResult(31)(Coordinate.fromSpiralCoordinate(1024).mannheimDistanceFromOrigin)
     }
 
-    test("347991") {
-        println("First solution for day three is: " + Coordinate.fromSpiralCoordinate(347991).mannheimDistanceFromOrigin)
+    test(input.toString) {
+        println("First solution for day three is: " + Coordinate.fromSpiralCoordinate(input).mannheimDistanceFromOrigin)
+    }
+
+    test("get value at origin") {
+        assertResult(1)(Coordinate.fromSpiralCoordinate(1).getValue)
+    }
+
+    test("values in first circle") {
+        assertResult(1)(Coordinate.fromSpiralCoordinate(2).getValue)
+        assertResult(2)(Coordinate.fromSpiralCoordinate(3).getValue)
+        assertResult(4)(Coordinate.fromSpiralCoordinate(4).getValue)
+        assertResult(5)(Coordinate.fromSpiralCoordinate(5).getValue)
+        assertResult(10)(Coordinate.fromSpiralCoordinate(6).getValue)
+        assertResult(11)(Coordinate.fromSpiralCoordinate(7).getValue)
+        assertResult(23)(Coordinate.fromSpiralCoordinate(8).getValue)
+        assertResult(25)(Coordinate.fromSpiralCoordinate(9).getValue)
+        assertResult(26)(Coordinate.fromSpiralCoordinate(10).getValue)
+
+    }
+
+    test(s"get first bigger value than $input") {
+        Coordinate.fromSpiralCoordinate(input) // Build coordinate map
+        val gettingValue: Int = Coordinate.m.values.map(_.getOrElse(0)).toList.sorted.find(_ > input).get
+
+        println(s"Second Result of day three is $gettingValue")
     }
 }
