38a78195358911828f491b4c81b0bbbb6773ce4c
Tobias Groschup <tobias.groschup@extoll.de>
2017-12-04 10:36:59 +0100

    Cleaner code for day four.

--- src/main/scala/Passphrase.scala
+++ src/main/scala/Passphrase.scala
@@ -1,17 +1,24 @@
 class Passphrase(input: String) {
     private val phrases: List[List[String]] = input.split("\n").toList.map(phrase => phrase.split(" ").toList)
 
-    private def checkPhrase(phrase: List[String]): Boolean = {
+    private def checkPhrase(phrase: List[String], isInvalid: (String, String) => Boolean): Boolean = {
         for(word <- phrase) {
-            var count = 0
+            var errorCount = 0
             for (other <- phrase) {
-                if(word == other) count += 1
-                if (count == 2) return false
+                if(isInvalid(word, other)) errorCount += 1
+                if (errorCount == 2) return false
             }
         }
         true
     }
 
-    def countValid: Int = phrases.foldRight(0)((phrase, result) => if(checkPhrase(phrase)) result + 1 else result)
+    private def isAnagram(s1: String, s2: String): Boolean = s1.sorted == s2.sorted
+
+    private def validCounter(phrases: List[List[String]], detectValidString: (String, String) => Boolean): Int =
+        phrases.foldRight(0)((phrase, result) => if(checkPhrase(phrase, detectValidString)) result + 1 else result)
+
+    def countValid: Int = validCounter(phrases, _ == _)
+    def countValidWithAnagrams: Int = validCounter(phrases, isAnagram)
 
 }
+
--- src/test/scala/ExecuteDay4.scala
+++ src/test/scala/ExecuteDay4.scala
@@ -522,19 +522,19 @@ class ExecuteDay4 extends FunSuite {
             |sryookh unrudn zrkz jxhrdo gctlyz
             |""".stripMargin
 
-    private val string1: String = s"aa bb cc dd ee\n"
-    private val string2: String = s"aa bb cc dd aa\n"
-    private val string3: String = s"aa bb cc dd aaa\n"
+    private val string1 = s"aa bb cc dd ee\n"
+    private val string2 = s"aa bb cc dd aa\n"
+    private val string3 = s"aa bb cc dd aaa\n"
 
-    test(string1) {
+    test("aa bb cc dd ee") {
         assertResult(1)(new Passphrase(string1).countValid)
     }
 
-    test(string2) {
+    test("aa bb cc dd aa") {
         assertResult(0)(new Passphrase(string2).countValid)
     }
 
-    test(string3) {
+    test("aa bb cc dd aaa") {
         assertResult(1)(new Passphrase(string3).countValid)
     }
 
@@ -543,6 +543,40 @@ class ExecuteDay4 extends FunSuite {
     }
 
     test("Solution one") {
-        println("First solution of day four: " + new Passphrase(input).countValid)
+        val result = new Passphrase(input).countValid
+        assertResult(383)(result)
+        println("First solution of day four: " + result)
+    }
+
+    private val string4 = "abcde fghij\n"
+    private val string5 = "abcde xyz ecdab\n"
+    private val string6 = "a ab abc abd abf abj\n"
+    private val string7 = "iiii oiii ooii oooi oooo\n"
+    private val string8 = "oiii ioii iioi iiio\n"
+
+    test("abcde fghij") {
+        assertResult(1)(new Passphrase(string4).countValidWithAnagrams)
+    }
+
+    test("abcde xyz ecdab") {
+        assertResult(0)(new Passphrase(string5).countValidWithAnagrams)
+    }
+
+    test("a ab abc abd abf abj") {
+        assertResult(1)(new Passphrase(string6).countValidWithAnagrams)
+    }
+
+    test("iiii oiii ooii oooi oooo") {
+        assertResult(1)(new Passphrase(string7).countValidWithAnagrams)
+    }
+
+    test("oiii ioii iioi iiio") {
+        assertResult(0)(new Passphrase(string8).countValidWithAnagrams)
+    }
+
+    test("Solution two") {
+        val result = new Passphrase(input).countValidWithAnagrams
+        assertResult(265)(result)
+        println("Second solution for day four is: " + result)
     }
 }
