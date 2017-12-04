f086748baa9a7f6f6b585e3989bc72cad7e779ef
Tobias Groschup <tobias.groschup@extoll.de>
2017-12-04 15:38:56 +0100

    Moved input for real results to resource files.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,15 @@
 object Main extends App {
-    println(s"hello world")
+    def getStringFromResource(name: String): String =
+        scala.io.Source.fromInputStream(getClass.getClassLoader.getResourceAsStream(name)).getLines().mkString("\n")
+
+    println("----------------------------")
+    println("---- Advent of Code 2017 ---")
+    println("----------------------------")
+    println("\nDay one:")
+    val inputDay1: String = getStringFromResource("day1")
+    println("First Solution: " + new CaptchaSolver(inputDay1).sum1)
+    println("Second Solution: " + new CaptchaSolver(inputDay1).sum2)
+
+    println("\nDay two:")
+
 }
