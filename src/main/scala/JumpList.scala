5d7f777e9c57a97d1b9213b26a0dd56d32f30dc6
Tobias Groschup <tobias.groschup@extoll.de>
2017-12-05 14:14:15 +0100

    Much improved performance.

--- src/main/scala/JumpList.scala
+++ src/main/scala/JumpList.scala
@@ -1,15 +1,31 @@
 class JumpList(jumpList: String) {
-    val jumps : List[Int]= jumpList.split("\n").map(java.lang.Integer.parseInt).toList
+    val jumps : Array[Int]= jumpList.split("\n").map(java.lang.Integer.parseInt)
 
-    private def jump(jumps: List[Int], i: Int, modifyIndex: Int => Int, jumpCount: Int = 0): Int =
-        if(i >= jumps.size || i < 0) jumpCount
-        else if(jumpCount < 0 ) {
-            println(s"More jumps than INT can hold, wtf")
-            System.exit(-1)
-            0
+    private def jump(jumps: Array[Int], i: Int, modifyIndex: Int => Int, jumpCounter: Int = 0): Int =
+        if(i >= jumps.length || i < 0) jumpCounter
+        else {
+            val newIndex = i + jumps(i)
+            jumps.update(i, modifyIndex(jumps(i)))
+            jump(jumps, newIndex, modifyIndex, jumpCounter +1)
         }
-        else jump(jumps.updated(i, modifyIndex(jumps(i))), i + jumps(i), modifyIndex, jumpCount +1)
+
+
+    private def jumpIterative(jumps: Array[Int], modifyIndex: Int => Int): Int = {
+        var jumpCounter = 0
+        var nextIndex = 0
+
+        while(nextIndex >= 0 && nextIndex < jumps.length) {
+            val current = nextIndex
+            nextIndex = nextIndex + jumps(current)
+            jumps(current) = modifyIndex(jumps(current))
+            jumpCounter += 1
+        }
+
+        jumpCounter
+    }
 
     def countJumps: Int = jump(jumps, 0, i => i + 1)
+    def countJumpsIterative: Int = jumpIterative(jumps, i => i + 1)
     def countJumpsInAnAbsurdWay: Int = jump(jumps, 0, i => if(i >= 3) i-1 else i + 1)
+    def countJumpsInAnAbsurdWayIterative: Int = jumpIterative(jumps, i => if(i >= 3) i-1 else i + 1)
 }
--- src/test/scala/Day5.scala
+++ src/test/scala/Day5.scala
@@ -13,12 +13,35 @@ class Day5 extends FunSuite{
         assertResult(5)(new JumpList(input).countJumps)
     }
 
+    test("example jumplist - iter") {
+        assertResult(5)(new JumpList(input).countJumpsIterative)
+    }
+
     test("real example") {
         val input = Main.getStringFromResource("day5")
         assertResult(372139)(new JumpList(input).countJumps)
     }
 
+    test("real example - iter") {
+        val input = Main.getStringFromResource("day5")
+        assertResult(372139)(new JumpList(input).countJumpsIterative)
+    }
+
     test("example jumplist counted strange") {
         assertResult(10)(new JumpList(input).countJumpsInAnAbsurdWay)
+    }
+
+    test("example jumplist counted strange but iterative") {
+        assertResult(10)(new JumpList(input).countJumpsInAnAbsurdWayIterative)
+    }
+
+    test("real example absurd jumps") {
+        val input = Main.getStringFromResource("day5")
+        assertResult(29629538)(new JumpList(input).countJumpsInAnAbsurdWay)
+    }
+
+    test("real example absurd jumps but iterative") {
+        val input = Main.getStringFromResource("day5")
+        assertResult(29629538)(new JumpList(input).countJumpsInAnAbsurdWayIterative)
     }
 }
